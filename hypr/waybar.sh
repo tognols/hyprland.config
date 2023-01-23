f298a596878354b77dcec09f3b5ad6b1489e1b8f
Matteo Tognolo <matteo@tognolo.it>
2023-01-23 11:44:20 +0100

    Aggiunto config (del lavoro)

---
+++ hypr/waybar.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# Terminate already running bar instances
+killall -q "waybar"
+killall -q "wbg"
+killall -q "polkit-gnome-authentication-agent-1"
+killall -q "network-trafic.sh"
+killall -q "dunst"
+
+# Wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+# Launch main
+wbg ~/Pictures/wal.jpg &
+polkit-gnome-authentication-agent-1 &
+waybar -c $HOME/.config/hypr/tokyo -s $HOME/.config/hypr/tokyo.css
+sleep 2 && pamixer --set-volume 70
